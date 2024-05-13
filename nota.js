48a6fa078a2f3293aa372a80922a644d6d8236c4
Gustavo Joseh <gustavojoseh2@gmail.com>
2024-05-13 18:46:57 -0300

    nota

---
+++ nota.js
@@ -0,0 +1,11 @@
+let nota1 = 8;
+let nota2 = 7.5;
+let nota3 = 9;
+let nota4 = 6.5;
+let nota5 = 8.5;
+
+let totalNotas = nota1 + nota2 + nota3 + nota4 + nota5;
+console.log(totalNotas);
+
+let mediaTotal = totalNotas / 5;
+console.log(mediaTotal);
