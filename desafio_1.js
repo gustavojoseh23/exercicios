099d585e94cf41703b67716ed1b5b606fb8d2193
Gustavo Joseh <gustavojoseh2@gmail.com>
2024-05-13 15:31:41 -0300

    primeiro commit

---
+++ desafio_1.js
@@ -0,0 +1,18 @@
+function operacoes(valor1, valor2) {
+    
+    console.log( valor1 + valor2);
+
+    console.log( valor1 - valor2);
+
+    console.log( valor1 * valor2);
+
+    if (valor2 !== 0) 
+    {
+        console.log(valor1 / valor2);
+    } else 
+    {
+        console.log("coloque um numero valido por favor");
+    }
+}
+
+operacoes (20, 0)
